3ba51fba05631a9bdc001e8213075b47cb329cfb
JihedGitlab <chaaben.jihed.gitlab@gmail.com>
2025-05-05 00:28:36 +0200

    ..

---
+++ src/main/java/com/chj/gr/config/WebConfig.java
@@ -0,0 +1,47 @@
+package com.chj.gr.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class ForSwaggerAggregatorConfig implements WebMvcConfigurer {
+//	
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/v3/api-docs/**")
+//                .allowedOrigins("http://localhost:8765")
+//                .allowedMethods("GET");
+//    }
+//}
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Value("${swagger.aggregator.cors.uri}")
+	private String swaggerAggregatorUri;		/** Penser à "gr-conf-swagger-aggregator". **/
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(swaggerAggregatorUri)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+        
+        /**
+         * Didn't work depuis gr-conf-swagger-aggregator..
+         */
+        // Configuration explicite pour les endpoints Actuator
+        registry.addMapping("/actuator/**")
+                .allowedOrigins(swaggerAggregatorUri)
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
