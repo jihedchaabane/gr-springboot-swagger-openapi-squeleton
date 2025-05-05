49d1b65cfc6ae5a10d553505cf508e3bbab9d273
JihedGitlab <chaaben.jihed.gitlab@gmail.com>
2025-05-05 17:36:43 +0200

    ..

--- src/main/java/com/chj/gr/config/WebConfig.java
+++ src/main/java/com/chj/gr/config/WebConfig.java
@@ -28,20 +28,25 @@ public class WebConfig implements WebMvcConfigurer {
 	private String swaggerAggregatorUri;		/** "gr-conf-swagger-aggregator". **/
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(swaggerAggregatorUri)	// Origine de "gr-conf-swagger-aggregator"
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+//        registry.addMapping("/**")
+//                .allowedOrigins(swaggerAggregatorUri)	// Origine de "gr-conf-swagger-aggregator"
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+    	
+		/**
+		 * Permettre toutes les origines (moins sécurisé, à utiliser pour le développement uniquement) :
+		 */
+    	registry.addMapping("/**").allowedOrigins("*");
         
         /**
          * @TODO Didn't work YET depuis gr-conf-swagger-aggregator..
          */
         // Configuration explicite pour les endpoints Actuator
-        registry.addMapping("/actuator/**")
-                .allowedOrigins(swaggerAggregatorUri)
-                .allowedMethods("GET", "POST", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+//        registry.addMapping("/actuator/**")
+//                .allowedOrigins(swaggerAggregatorUri)
+//                .allowedMethods("GET", "POST", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
     }
 }
