da90231799635ab45954ca91d1324d2e567298a4
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-30 13:26:39 +0200

    common-params instead of common-configs

--- src/main/java/com/chj/gr/controller/GetController.java
+++ src/main/java/com/chj/gr/controller/GetController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.chj.gr.properties.SwaggerParamsProperties;
+import com.chj.gr.config.properties.SwaggerParamsProperties;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
